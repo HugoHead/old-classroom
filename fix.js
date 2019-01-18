c7cb080faada43205ab4ff98dc8093b81740577f
HugoHead <45836267+HugoHead@users.noreply.github.com>
2019-01-17 21:48:34 -0500

    Add files via upload

---
+++ fix.js
@@ -0,0 +1,11 @@
+document.getElementsByTagName("NAV")[0].style.backgroundColor = "#4b4b4b";
+document.getElementsByClassName("XIpEib QRiHXd")[0].style.backgroundColor = "#4b4b4b";
+document.getElementsByClassName("GmuOkf")[0].style.fill = "white"
+document.getElementsByClassName("xSP5ic")[0].style.fill = "rgb(255,255,255)"
+document.getElementsByClassName("yEedwb")[0].innerHTML = "<span class=\"IqJTee\"><em><strong><b>Google </em></strong></b>Classroom</span>"
+document.getElementsByClassName("IqJTee")[0].style.color = "white";
+document.getElementsByTagName("BODY")[0].style.backgroundColor = "#eeeeee"
+    var elems = document.getElementsByClassName("gHz6xd");
+    for (var i = 0; i < elems.length; i++) {
+        elems[i].style.borderRadius = "0";
+    }
