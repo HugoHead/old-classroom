de17331e226d73c01e860d2b147c1e989bff96ee
HugoHead <45836267+HugoHead@users.noreply.github.com>
2019-01-18 16:35:44 -0500

    Update fix-main.js

--- fix-main.js
+++ fix-main.js
@@ -5,13 +5,17 @@ document.getElementsByClassName("xSP5ic")[0].style.fill = "rgb(255,255,255)"
 document.getElementsByClassName("yEedwb")[0].innerHTML = "<span class=\"IqJTee\"><em><strong><b>Google </em></strong></b>Classroom</span>"
 document.getElementsByClassName("IqJTee")[0].style.color = "white";
 document.getElementsByTagName("BODY")[0].style.backgroundColor = "#eeeeee"
-    var elems = document.getElementsByClassName("gHz6xd");
-    for (var i = 0; i < elems.length; i++) {
-        elems[i].style.borderRadius = "0";
-    }
+var elems = document.getElementsByClassName("gHz6xd");
+for (var i = 0; i < elems.length; i++) {
+    elems[i].style.borderRadius = "0";
+}
 document.getElementsByClassName("gb_kf")[0].style.opacity = "100";
 document.getElementsByClassName("gb_kf")[0].style.fill = "rgb(255,255,255)";
 document.getElementsByClassName("gb_lf")[0].style.opacity = "100";
 document.getElementsByClassName("gb_lf")[0].style.fill = "rgb(255,255,255)";
 //help button accsessor: document.getElementsByClassName("Lw7GHd")[document.getElementsByClassName("Lw7GHd").length - 1]
 document.getElementsByClassName("Lw7GHd")[document.getElementsByClassName("Lw7GHd").length - 1].innerHTML = "<img src='https://raw.githubusercontent.com/HugoHead/old-classroom/master/old%20Clssroom/helpicon.png'>"; 
+elems = document.getElementsByClassName("GmuOkf");
+for (var i = 0; i < elems.length; i++) {
+        elems[i].innerHTML = '<img src="https://raw.githubusercontent.com/HugoHead/old-classroom/master/old%20Clssroom/classworkFolderIcon.png">';   
+}
