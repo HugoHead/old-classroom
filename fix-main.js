e4b5bb49a79d540c53d3001444b172d3be773494
HugoHead <45836267+HugoHead@users.noreply.github.com>
2019-01-18 16:51:37 -0500

    Update fix-main.js
    Need to fix the right icons.

--- fix-main.js
+++ fix-main.js
@@ -1,11 +1,16 @@
 document.getElementsByTagName("NAV")[0].style.backgroundColor = "#4b4b4b";
 document.getElementsByClassName("XIpEib QRiHXd")[0].style.backgroundColor = "#4b4b4b";
+var elems = document.getElementsByClassName("GmuOkf");
+for (var i = 0; i < elems.length; i++) {
+        elems[i].innerHTML = '<img src="https://raw.githubusercontent.com/HugoHead/old-classroom/master/old%20Clssroom/classworkFolderIcon.png">';   
+}
+document.getElementsByClassName("sEZiv")[0].innerHTML = '<div class="PDXc1b MbhUzd" jsname="ksKsZd"></div><content class="XuQwKc"><span class="GmuOkf" style="top: -12px"><svg focusable="false" xmlns="https://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 13h-7v7h-2v-7H4v-2h7V4h2v7h7v2z"></path></svg></span></content>';
 document.getElementsByClassName("GmuOkf")[0].style.fill = "white"
 document.getElementsByClassName("xSP5ic")[0].style.fill = "rgb(255,255,255)"
 document.getElementsByClassName("yEedwb")[0].innerHTML = "<span class=\"IqJTee\"><em><strong><b>Google </em></strong></b>Classroom</span>"
 document.getElementsByClassName("IqJTee")[0].style.color = "white";
 document.getElementsByTagName("BODY")[0].style.backgroundColor = "#eeeeee"
-var elems = document.getElementsByClassName("gHz6xd");
+elems = document.getElementsByClassName("gHz6xd");
 for (var i = 0; i < elems.length; i++) {
     elems[i].style.borderRadius = "0";
 }
@@ -14,8 +19,4 @@ document.getElementsByClassName("gb_kf")[0].style.fill = "rgb(255,255,255)";
 document.getElementsByClassName("gb_lf")[0].style.opacity = "100";
 document.getElementsByClassName("gb_lf")[0].style.fill = "rgb(255,255,255)";
 //help button accsessor: document.getElementsByClassName("Lw7GHd")[document.getElementsByClassName("Lw7GHd").length - 1]
-document.getElementsByClassName("Lw7GHd")[document.getElementsByClassName("Lw7GHd").length - 1].innerHTML = "<img src='https://raw.githubusercontent.com/HugoHead/old-classroom/master/old%20Clssroom/helpicon.png'>"; 
-elems = document.getElementsByClassName("GmuOkf");
-for (var i = 0; i < elems.length; i++) {
-        elems[i].innerHTML = '<img src="https://raw.githubusercontent.com/HugoHead/old-classroom/master/old%20Clssroom/classworkFolderIcon.png">';   
-}
+document.getElementsByClassName("Lw7GHd")[document.getElementsByClassName("Lw7GHd").length - 1].innerHTML = "<img src='https://raw.githubusercontent.com/HugoHead/old-classroom/master/old%20Clssroom/helpicon.png'>";
