4417630ca58d925e9b18c7894c17dc366c781855
HugoHead <45836267+HugoHead@users.noreply.github.com>
2019-01-18 20:30:25 -0500

    Update fix-stream.js

--- fix-stream.js
+++ fix-stream.js
@@ -1,1 +1,3 @@
-
+//change navbar color
+document.getElementsByTagName("NAV")[0].style.backgroundColor = "rgb(75,75,75)";
+document.getElementsByClassName("XIpEib")[0].style.backgroundColor = "rgb(75,75,75)";
