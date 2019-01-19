6e4bbc804df92dc9c9b79bb4bc8c39e8af4ba6c9
HugoHead <45836267+HugoHead@users.noreply.github.com>
2019-01-19 18:42:52 -0500

    Update wip-tampermonkey.js
    Made it work better.

--- wip-tampermonkey.js
+++ wip-tampermonkey.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://classroom.google.com/u/*/h*
+// @match        https://classroom.google.com/*
 // @grant        none
 // ==/UserScript==
 
