78d48977480b0c9434d7d09721845c2a25eeb4a4
HugoHead <45836267+HugoHead@users.noreply.github.com>
2019-01-20 18:18:58 -0500

    Update fix-stream.js
    Makes all of the posts have a borderRdius of 0.

--- fix-stream.js
+++ fix-stream.js
@@ -1,3 +1,8 @@
 //change navbar color
 document.getElementsByTagName("NAV")[0].style.backgroundColor = "rgb(75,75,75)";
 document.getElementsByClassName("XIpEib")[0].style.backgroundColor = "rgb(75,75,75)";
+
+var elems = document.getElementsByClassName("qhnNic");
+for (var i = 0; i < elems.length; i++) {
+    elems[i].style.borderRadius = "0";
+}
