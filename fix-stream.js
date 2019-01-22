fc28c66beff4ad1c1cee960cca2ab2aa073cdf71
HugoHead <45836267+HugoHead@users.noreply.github.com>
2019-01-22 16:32:05 -0500

    Update fix-stream.js
    Add a few coments.

--- fix-stream.js
+++ fix-stream.js
@@ -1,13 +1,15 @@
-//change navbar color
-document.getElementsByTagName("NAV")[0].style.backgroundColor = "rgb(75,75,75)";
-document.getElementsByClassName("XIpEib")[0].style.backgroundColor = "rgb(75,75,75)";
-
+//unround the corrners
 var elems = document.getElementsByClassName("qhnNic");
 for (var i = 0; i < elems.length; i++) {
     elems[i].style.borderRadius = "0";
 }
 
+//get the darker class color
 var elem,style;
 elem = document.querySelector('.VnOHwf-Tvm9db');
 style = getComputedStyle(elem);
 var classColor = style.color;
+
+//chnage the color of the navbar to the darker class color
+document.getElementsByTagName("NAV")[0].style.backgroundColor = classColor;
+document.getElementsByClassName("XIpEib")[0].style.backgroundColor = classColor;
