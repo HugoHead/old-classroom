3a6fb9ebbc67504cc50627e8dc3ee0ddc415479b
HugoHead <45836267+HugoHead@users.noreply.github.com>
2019-01-22 16:26:55 -0500

    Update fix-stream.js
    Added a var the access the color of the classroom color.

--- fix-stream.js
+++ fix-stream.js
@@ -6,3 +6,8 @@ var elems = document.getElementsByClassName("qhnNic");
 for (var i = 0; i < elems.length; i++) {
     elems[i].style.borderRadius = "0";
 }
+
+var elem,style;
+elem = document.querySelector('.VnOHwf-Tvm9db');
+style = getComputedStyle(elem);
+var classColor = style.color;
