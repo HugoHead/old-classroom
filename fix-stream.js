8813d28046a12d6beb669cbe683afd5c229d98fd
HugoHead <45836267+HugoHead@users.noreply.github.com>
2019-01-25 09:45:21 -0500

    Update fix-stream.js

--- fix-stream.js
+++ fix-stream.js
@@ -21,6 +21,7 @@ document.getElementsByClassName("xSP5ic")[0].style.fill = "white";
 var className = document.getElementById("UGb2Qe").innerHTML;
 document.getElementById("UGb2Qe").addEventListener("mouseover", function(){document.getElementById("UGb2Qe").style.textDecoration = "underline"; console.log("on");});
 document.getElementById("UGb2Qe").addEventListener("mouseout", function(){document.getElementById("UGb2Qe").style.textDecoration = "none"; console.log("off");});
+document.getElementsByClassName("HDsjVd")[0].style.color = "white"
 
 // fix the work to do box on the right.
 document.getElementsByClassName("d4Fe0d")[0].style.borderRadius = "0";
