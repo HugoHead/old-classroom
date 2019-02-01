75900cf83aa0c36d122f28db683030204f8d48e1
HugoHead <45836267+HugoHead@users.noreply.github.com>
2019-02-01 13:54:36 -0500

    Update wip-tampermonkey.js

--- wip-tampermonkey.js
+++ wip-tampermonkey.js
@@ -11,15 +11,13 @@
 (function() {
     'use strict';
 window.addEventListener('load', function(){
-    //google seems to be doing some kind of nonsense whre they download each part of the page individually then render all of it.
-    //THis means that when our script takes effect, all of the page may or may not be rendered.
-    //This is a huge issue.
-    //Solutions that have been tried:
-    //Adding delay, does not work, reason is unknown
-    //looping, causes page to crash
-    //looping with delay, same as above.
-    console.log("here");
     var site = document.location.href;
+    console.log("here");
+    console.log("here15");
+    console.log(site);
+    console.log(site.split("/"));
+    console.log(site.split("/")[site.split("/").length - 1]);
+    console.log(site.split("/")[site.split("/").length - 1] == "details")
     if (site == "https://classroom.google.com/u/0/h" || site == "https://classroom.google.com/u/1/h" || site == "https://classroom.google.com/u/2/h"
     || site == "https://classroom.google.com/u/3/h" || site == "https://classroom.google.com/u/4/h" ||
     site == "https://classroom.google.com/u/5/h" || site == "https://classroom.google.com/u/5/h" ||
@@ -37,6 +35,13 @@ window.addEventListener('load', function(){
 		fixStream();
 		console.log("here14");
 	}
+    else if (site.split("/")[site.split("/").length - 1] == "details")
+    {
+        console.log("here16")
+        window.setTimeout(function(){delay(150); fixAssignment();}, 1700);
+		fixAssignment();
+		console.log("here14");
+    }
 });
 function delay(ms) {
     var cur_d = new Date();
@@ -48,6 +53,34 @@ function delay(ms) {
         ms_passed = ticks - cur_ticks;
         // d = null;  people also siad this may prevent the memory leak.
     }
+}
+const pSBC = function (p, from, to) {
+if(typeof(p)!="number"||p<-1||p>1||typeof(from)!="string"||(from[0]!='r'&&from[0]!='#')||(to&&typeof(to)!="string"))return null; //ErrorCheck
+if(!this.pSBCr)this.pSBCr=(d)=>{
+    let l=d.length,RGB={};
+    if(l>9){
+        d=d.split(",");
+        if(d.length<3||d.length>4)return null;//ErrorCheck
+        RGB[0]=i(d[0].split("(")[1]),RGB[1]=i(d[1]),RGB[2]=i(d[2]),RGB[3]=d[3]?parseFloat(d[3]):-1;
+    }else{
+        if(l==8||l==6||l<4)return null; //ErrorCheck
+        if(l<6)d="#"+d[1]+d[1]+d[2]+d[2]+d[3]+d[3]+(l>4?d[4]+""+d[4]:""); //3 or 4 digit
+        d=i(d.slice(1),16),RGB[0]=d>>16&255,RGB[1]=d>>8&255,RGB[2]=d&255,RGB[3]=-1;
+        if(l==9||l==5)RGB[3]=r((RGB[2]/255)*10000)/10000,RGB[2]=RGB[1],RGB[1]=RGB[0],RGB[0]=d>>24&255;
+    }
+    return RGB;}
+var i=parseInt,
+    r=Math.round,
+    h=from.length>9;
+    h=typeof(to)=="string"?to.length>9?true:to=="c"?!h:false:h;
+    var b = p<0;
+    p=b?p*-1:p;
+    to=to&&to!="c"?to:b?"#000000":"#FFFFFF";
+    var f = this.pSBCr(from);
+    var t = this.pSBCr(to);
+if(!f||!t)return null; //ErrorCheck
+if(h)return "rgb"+(f[3]>-1||t[3]>-1?"a(":"(")+r((t[0]-f[0])*p+f[0])+","+r((t[1]-f[1])*p+f[1])+","+r((t[2]-f[2])*p+f[2])+(f[3]<0&&t[3]<0?")":","+(f[3]>-1&&t[3]>-1?r(((t[3]-f[3])*p+f[3])*10000)/10000:t[3]<0?f[3]:t[3])+")");
+else return "#"+(0x100000000+r((t[0]-f[0])*p+f[0])*0x1000000+r((t[1]-f[1])*p+f[1])*0x10000+r((t[2]-f[2])*p+f[2])*0x100+(f[3]>-1&&t[3]>-1?r(((t[3]-f[3])*p+f[3])*255):t[3]>-1?r(t[3]*255):f[3]>-1?r(f[3]*255):255)).toString(16).slice(1,f[3]>-1||t[3]>-1?undefined:-2);
 }
 function fixMain()
     {
@@ -94,7 +127,7 @@ function fixMain()
         //help button accsessor: document.getElementsByClassName("Lw7GHd")[document.getElementsByClassName("Lw7GHd").length - 1]
         document.getElementsByClassName("Lw7GHd")[document.getElementsByClassName("Lw7GHd").length - 1].innerHTML = "<img src='https://raw.githubusercontent.com/HugoHead/old-classroom/master/old%20Clssroom/helpicon.png'>";
         console.log("here13");
-		for (var i = 0; i < document.getElementsByClassName("nk37z").length; i++) {
+		for (i = 0; i < document.getElementsByClassName("nk37z").length; i++) {
 			document.getElementsByClassName("nk37z")[i].addEventListener("click", function(){
 					var target = document.getElementsByClassName("nk37z")[i].href;
 				});
@@ -180,8 +213,14 @@ imageElement.style.height = "240px";
 //remove the stupdi arrow
 document.getElementsByClassName("GmuOkf")[1].parentNode.removeChild(document.getElementsByClassName("GmuOkf")[1]);
 
-//get rid of that obnoxios white bar
+//remvoe the stupid white bar
 document.getElementsByClassName("joJglb")[0].style.borderBottom = ".1rem solid " + classColor;
+
+//change the background color of the class
+document.getElementsByTagName("BODY")[0].style.backgroundColor = pSBC(0.87,classColor);
+
+//make the work to do box transparant but with an opacity
+document.getElementsByClassName("d4Fe0d")[0].style.backgroundColor = "";
 
 //make sure this is last:
 //If it is not last, it will throw error and void the rest of the script. MAKE IT LAST!
@@ -190,4 +229,70 @@ document.getElementsByClassName("HDsjVd")[0].style.color = "white";
 document.getElementsByClassName("HDsjVd")[0].addEventListener("mouseover", function(){document.getElementsByClassName("HDsjVd")[0].style.textDecoration = "underline"; console.log("on");});
 document.getElementsByClassName("HDsjVd")[0].addEventListener("mouseout", function(){document.getElementsByClassName("HDsjVd")[0].style.textDecoration = "none"; console.log("off");});
 	}
+function fixAssignment()
+    {
+        console.log("here17");
+        //get the darker classColor
+var elem,style,classColor;
+elem = document.querySelector(".p0oLxb:not(.RDPZE).Rp4yn");
+style = getComputedStyle(elem);
+classColor = style.color;
+
+
+//chnage the navbar color
+document.getElementsByTagName("NAV")[0].style.backgroundColor = classColor;
+document.getElementsByClassName("XIpEib")[0].style.backgroundColor = classColor;
+
+//change the color of the class name
+document.getElementById("UGb2Qe").style.color = "white"
+
+//change the color of the three dots
+document.getElementsByClassName("xSP5ic")[0].style.fill = "white";
+
+//add an underline on hover for the class
+var className = document.getElementById("UGb2Qe").innerHTML;
+document.getElementById("UGb2Qe").addEventListener("mouseover", function(){document.getElementById("UGb2Qe").style.textDecoration = "underline"; console.log("on");});
+document.getElementById("UGb2Qe").addEventListener("mouseout", function(){document.getElementById("UGb2Qe").style.textDecoration = "none"; console.log("off");});
+
+//flip the color sceme of the help icon
+document.getElementsByClassName("Lw7GHd")[document.getElementsByClassName("Lw7GHd").length - 1].innerHTML = "<img src='https://raw.githubusercontent.com/HugoHead/old-classroom/master/old%20Clssroom/helpicon.png'>";
+
+//change the icon colors
+document.getElementsByClassName("gb_kf")[0].style.opacity = "100";
+document.getElementsByClassName("gb_kf")[0].style.fill = "rgb(255, 255,255)";
+document.getElementsByClassName("gb_lf")[0].style.opacity = "100";
+document.getElementsByClassName("gb_lf")[0].style.fill = "rgb(255,255,255)";
+
+//chnage the color of the middle bar.
+document.getElementsByClassName("nl5VRd")[0].style.backgroundColor = pSBC(0.17, classColor);
+
+//make all the text on the middle bar white
+document.getElementsByClassName("ktcYN")[0].childNodes[0].style.color = "white";
+document.getElementsByClassName("fOvfyc")[0].style.color = "white";
+document.getElementsByClassName("z4oNBf")[0].style.color = "white";
+document.getElementsByClassName("Socxke")[1].style.color = "white";
+document.getElementsByClassName("Socxke")[1].style.color = "white";
+document.getElementsByClassName("VMvujb")[0].style.color = "white";
+
+//remove the stupid white bar
+document.getElementsByClassName("joJglb")[0].style.borderBottom = ".1rem solid " + classColor;
+
+//add a box shadow to the card
+document.getElementsByClassName("yckQJf")[0].style.boxShadow = "0 1px 2px 0 rgba(60,64,67,0.302),0 2px 6px 2px rgba(60,64,67,0.149)";
+
+//fix the border radius of the submit button
+document.getElementsByClassName("U26fgb")[3].style.backgroudColor = "0";
+
+document.getElementsByClassName("u155de")[1].style.backgroundColor = pSBC(0.9, classColor);
+document.getElementsByTagName("BODY")[0].style.backgroundColor = pSBC(0.9, classColor);
+
+document.getElementsByClassName("gMIble")[0].remove();
+
+//keep at bottom
+document.getElementsByClassName("HDsjVd")[0].style.color = "white";
+document.getElementsByClassName("HDsjVd")[0].addEventListener("mouseover", function(){document.getElementsByClassName("HDsjVd")[0].style.textDecoration = "underline"; console.log("on");});
+document.getElementsByClassName("HDsjVd")[0].addEventListener("mouseout", function(){document.getElementsByClassName("HDsjVd")[0].style.textDecoration = "none"; console.log("off");});
+
+document.getElementsByClassName("uqZtlf")[0].style.backgroundColor = "white";
+    }
 })();
